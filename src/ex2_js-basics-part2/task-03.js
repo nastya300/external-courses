2c848391d12b5770b32c21218b0c646652fd5819
Anastasia <nastya.lobakina@icloud.com>
2021-09-18 10:51:49 +0300

    выполнены задания task01-task06

---
+++ src/ex2_js-basics-part2/task-03.js
@@ -0,0 +1,18 @@
+function printEvenAndOddElementsArray(inputArray) {
+  let outputArray = [0, 0, 0];
+  for (let i = 0; i < inputArray.length; i += 1) {
+    if (
+      inputArray[i] % 2 === 0 &&
+      inputArray[i] !== 0 &&
+      inputArray[i] !== null
+    ) {
+      outputArray[0] += 1;
+    } else if (inputArray[i] % 2 !== 0) {
+      outputArray[1] += 1;
+    } else if (inputArray[i] === 0) {
+      outputArray[2] += 1;
+    }
+  }
+  return outputArray;
+}
+module.exports = printEvenAndOddElementsArray;
