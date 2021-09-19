c58b76219a5d9e8a92f3bc3471650bec4daf0eeb
Anastasia <nastya.lobakina@icloud.com>
2021-09-19 11:27:43 +0300

    добавлена 6

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,3 +1,12 @@
-function deepClone(){
-    
+function deepClone(obj) {
+  const clObj = {};
+  for (const i in obj) {
+    if (obj[i] instanceof Object) {
+      clObj[i] = deepClone(obj[i]);
+      continue;
+    }
+    clObj[i] = obj[i];
+  }
+  return clObj;
 }
+module.exports = deepClone;
