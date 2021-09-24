6f7425b63d79e9d2050ecfaec6d0c33effd3fa61
Anastasia <nastya.lobakina@icloud.com>
2021-09-24 20:47:34 +0300

    увц

--- src/ex4_js-objects-part2/task-08.js
+++ src/ex4_js-objects-part2/task-08.js
@@ -1,7 +1,7 @@
 function getStringLowerCamelCase(string) {
   string = string.split(" ");
   string[0] = string[0].toLowerCase();
-  for (let i = 1; i < str.length; i += 1) {
+  for (let i = 1; i < string.length; i += 1) {
     string[i] = string[i].charAt(0).toUpperCase() + string[i].substring(1);
   }
   return string.join("");
