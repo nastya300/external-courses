8e38c8b00429a003df2c8ee8c74922888720031a
Anastasia <nastya.lobakina@icloud.com>
2021-09-26 17:52:47 +0300

    fixed 04

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -2,7 +2,7 @@ function newKey(propertyName, obj) {
   let objCopy = {};
   let keys = Object.keys(obj);
   for (let i = 0; i < keys.length; i += 1) {
-    objCopy[i] = obj[i];
+    objCopy[keys[i]] = obj[keys[i]];
   }
   if (!obj.hasOwnProperty(propertyName)) {
     objCopy[propertyName] = "new";
