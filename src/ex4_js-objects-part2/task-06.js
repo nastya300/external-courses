bdc880d4018967b82d2a0bc3ac0c1bc3646daeba
Anastasia <nastya.lobakina@icloud.com>
2021-09-26 21:19:05 +0300

    21:19

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,7 +1,8 @@
 function getStrUppercaseCharacterEachWord(string) {
   let resultString = string.split(" ");
-  for (let i = 0; i < res.length; i += 1) {
-    resultString[i] = resultString[i].charAt(0).toUpperCase() + resultString[i].substring(1);
+  for (let i = 0; i < resultString.length; i += 1) {
+    resultString[i] =
+      resultString[i].charAt(0).toUpperCase() + resultString[i].substring(1);
   }
   return resultString.join(" ");
 }
--- src/ex4_js-objects-part2/task-08.js
+++ src/ex4_js-objects-part2/task-08.js
@@ -1,11 +1,12 @@
-function  getStringLowerCamelCase(str){
-let resultString=str.split(' ');
-resultString[0]=resultString[0].toLowerCase();
-  for (let i=1; i< resultString.length;i+=1){
-    resultString[i] = resultString[i].charAt(0).toUpperCase() + resultString[i].substring(1).toLowerCase();
+function getStringLowerCamelCase(str) {
+  let resultString = str.split(" ");
+  resultString[0] = resultString[0].toLowerCase();
+  for (let i = 1; i < resultString.length; i += 1) {
+    resultString[i] =
+      resultString[i].charAt(0).toUpperCase() +
+      resultString[i].substring(1).toLowerCase();
   }
-  return resultString.join('');
+  return resultString.join("");
 }
 
 module.exports = getStringLowerCamelCase;
-
