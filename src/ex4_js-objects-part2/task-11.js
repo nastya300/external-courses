f986dccc6775bd00b3d4861185663bedbd4e444f
Anastasia <nastya.lobakina@icloud.com>
2021-09-27 12:18:49 +0300

    add 11

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,3 +1,15 @@
+
 function countEachCharacterString(str){
-    
+let map={};
+for (let i=0; i<str.length;i++){
+    if (map.hasOwnProperty(str[i])){
+        map[str[i]]++;
+    }else {
+        map[str[i]]=1;
+    }
 }
+   for (let key in map){
+       console.log(key,map[key]);
+   }
+}
+module.exports=countEachCharacterString;
