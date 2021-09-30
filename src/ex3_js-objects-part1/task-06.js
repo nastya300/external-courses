2a6596fd1b12f0fcd61d0c44e6bd7ec990ffc7fc
Anastasia <nastya.lobakina@icloud.com>
2021-10-01 00:52:32 +0300

    fix 6 01.10

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,29 +1,25 @@
 function deepCloneObject(obj) {
-  const clone = Object.assign({}, obj);
-
-  Object.keys(clone).forEach(function check(key) {
-    if (typeof obj[key] === "object") {
-      clone[key] = deepCloneObject(obj[key]);
-    } else {
-      clone[key] = obj[key];
-    }
-  });
+  let clone;
   if (Array.isArray(obj)) {
+    clone = Object.assign({}, obj);
+    Object.keys(clone).forEach(function check(key) {
+      if (typeof obj[key] === "object") {
+        clone[key] = deepCloneObject(obj[key]);
+      } else {
+        clone[key] = obj[key];
+      }
+    });
+  } else {
+    clone = Object.assign([], obj);
     if (obj.length) {
-      clone.length = obj.length;
       for (let i = 0; i < obj.length; i += 1) {
         if (typeof obj[i] === "object") {
           clone[i] = deepCloneObject(obj[i]);
         }
       }
-      return Array.from(clone);
-    } else {
-      return Array.from(obj);
     }
-  } else {
-    return clone;
   }
+  return clone;
 }
 
 module.exports = deepCloneObject;
-
