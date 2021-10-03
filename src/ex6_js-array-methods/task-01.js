ca4e7d649dd98277788190ed7aa277fd55b0e5b7
Anastasia <nastya.lobakina@icloud.com>
2021-10-03 11:18:26 +0300

    linter 1

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,24 +1,27 @@
 function slice(array, start, end) {
+  let newStart = start;
+  let NewEnd = end;
   let clone = [];
-  if (start < 0) {
-    start = array.length + start;
-    if (start < 0) {
-      start = 0;
+
+  if (newStart < 0) {
+    newStart = array.length + newStart;
+    if (newStart < 0) {
+      newStart = 0;
     }
   }
-  if (end < 0) {
-    end = array.length + end;
+  if (NewEnd < 0) {
+    NewEnd = array.length + NewEnd;
   }
-  if (end > array.length) {
-    end = array.length;
+  if (NewEnd > array.length) {
+    NewEnd = NewEnd.length;
   }
 
   if (arguments.length === 3) {
-    for (let i = start; i < end; i += 1) {
+    for (let i = newStart; i < NewEnd; i += 1) {
       clone.push(array[i]);
     }
   } else if (arguments.length === 2) {
-    for (let i = start; i < array.length; i += 1) {
+    for (let i = newStart; i < array.length; i += 1) {
       clone.push(array[i]);
     }
   } else {
