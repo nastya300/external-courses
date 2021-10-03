6c3a42580846a30f15278b5da9d12e9e09c9e3a5
Anastasia <nastya.lobakina@icloud.com>
2021-10-03 13:33:09 +0300

    add startIndex

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -6,7 +6,7 @@ function reduce(array, callback, initialValue){
     }else {
         startIndex = 1;
     }
-        for (let i=0; i < array.length; i += 1){
+        for (let i=startIndex; i < array.length; i += 1){
             previousValue=callback(previousValue,array[i],i,array);
         }
     return previousValue;
