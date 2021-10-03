89e382cbb53e84a0099f9bac4f809b3f150ccbc8
Anastasia <nastya.lobakina@icloud.com>
2021-10-03 13:34:35 +0300

    add style

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,15 +1,16 @@
-function reduce(array, callback, initialValue){
-    let startIndex=0;
-    let previousValue=array[0];
-    if(arguments.length===3) {
-        previousValue = initialValue;
-    }else {
-        startIndex = 1;
-    }
-        for (let i=startIndex; i < array.length; i += 1){
-            previousValue=callback(previousValue,array[i],i,array);
-        }
-    return previousValue;
+function reduce(array, callback, initialValue) {
+  let startIndex = 0;
+  let previousValue = array[0];
+  
+  if (arguments.length === 3) {
+    previousValue = initialValue;
+  } else {
+    startIndex = 1;
+  }
+  for (let i = startIndex; i < array.length; i += 1) {
+    previousValue = callback(previousValue, array[i], i, array);
+  }
+  return previousValue;
 }
 
 module.exports = reduce;
