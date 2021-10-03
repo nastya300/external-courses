b9c80cf06b0f5a06e7e1d7c214704f87b2ce5b36
Anastasia <nastya.lobakina@icloud.com>
2021-10-03 13:31:19 +0300

    add t

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,3 +1,15 @@
-function d(){
-    
+function reduce(array, callback, initialValue){
+    let startIndex=0;
+    let previousValue=array[0];
+    if(arguments.length===3) {
+        previousValue = initialValue;
+    }else {
+        startIndex = 1;
+    }
+        for (let i=0; i < array.length; i += 1){
+            previousValue=callback(previousValue,array[i],i,array);
+        }
+    return previousValue;
 }
+
+module.exports = reduce;
