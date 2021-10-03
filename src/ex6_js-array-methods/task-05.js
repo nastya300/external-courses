5c3a360d5b9aaf7cac67b5cbe08650a51189652d
Anastasia <nastya.lobakina@icloud.com>
2021-10-03 10:32:27 +0300

    fix 5

--- src/ex6_js-array-methods/task-05.js
+++ src/ex6_js-array-methods/task-05.js
@@ -1,7 +1,7 @@
 function map(array, callback) {
   let clone = [];
-  for (let i = 0; i < array.length; i++) {
-    clone.push(callback(array[i]),i, array);
+  for (let i = 0; i < array.length; i+=1) {
+    clone.push(callback(array[i],i,array));
   }
   return clone;
 }
