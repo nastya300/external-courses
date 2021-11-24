faf01c9b271dd367e482b1284c1bc3242d023d8d
Anastasia <nastya.lobakina@icloud.com>
2021-11-24 14:41:28 +0300

    отбивка

--- src/ex12_js_dom/task.js
+++ src/ex12_js_dom/task.js
@@ -5,16 +5,17 @@ let array = [
   "assests/4.jpg",
   "assests/5.jpg",
 ];
-let section = document.getElementById("section");
-let next = document.getElementById("next");
-let back = document.getElementById("back");
+const section = document.getElementById("section");
+const next = document.getElementById("next");
+const back = document.getElementById("back");
+let current = +section.getAttribute("data-current");
 
 next.addEventListener("click", function lis() {
-  var current = +section.getAttribute("data-current");
   current++;
   if (current >= array.length) {
     current = 0;
   }
+
   section.setAttribute("data-current", current);
   section.innerHTML = "<img  src='" + array[current] + "'/>";
 });
@@ -25,6 +26,7 @@ back.addEventListener("click", function () {
   if (current < 0) {
     current = array.length - 1;
   }
+  
   section.setAttribute("data-current", current);
   section.innerHTML = "<img src='" + array[current] + "'/>";
 });
